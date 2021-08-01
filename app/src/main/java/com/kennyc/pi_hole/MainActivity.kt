801c53ba71e33d7833476e132d886c97b3c2aabc
Kenny <kennyc1012@gmail.com>
2021-08-01 10:25:24 -0400

    Remove unused imports

--- app/src/main/java/com/kennyc/pi_hole/MainActivity.kt
+++ app/src/main/java/com/kennyc/pi_hole/MainActivity.kt
@@ -6,12 +6,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
-import com.kennyc.data.pi_hole.model.PiholeSummary
-import com.kennyc.data.pi_hole.model.PiholeSystemStatus
-import com.kennyc.pi_hole.ui.theme.PiUi.buildUi
 import com.kennyc.pi_hole.ui.theme.PiUi.piholeScreen
 import com.kennyc.pi_hole.ui.theme.PiholeComposeTheme
 import javax.inject.Inject
